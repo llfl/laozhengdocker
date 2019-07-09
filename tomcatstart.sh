5580004e622bcedbee81f9c4ea392295764f07b1
Kun <quinnHugh@hotmail.com>
2019-07-09 08:28:37 +0800

    update

--- tomcatstart.sh
+++ tomcatstart.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo docker run -it --rm -p 8080:8080 --mount source=/home/longshuz/trashService/tmp,target=/tmp/arcam llfl-tomcat
+docker build docker-tomcat -t llfl-tomcat
+
+docker run -it --rm -p 8080:8080 -v /home/longshuz/trashService/tmp:/tmp/arcam llfl-tomcat
